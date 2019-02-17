8845444e905848877813310bf64e4f398d098ace
BryanU10 <43707976+BryanU10@users.noreply.github.com>
2019-02-17 12:46:47 -0600

    Agregando Repositorio Generico y Unidad de Trabajo

---
+++ ProyectoFinal/BackEnd/DAL/UnidadDeTrabajo.cs
@@ -0,0 +1,33 @@
+﻿using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAL
+{
+    public class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+        private readonly BDContext context;
+        public IDALGenerico<T> genericDAL;
+
+
+        public UnidadDeTrabajo(BDContext _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericoImpl<T>(context);
+        }
+
+        public void Complete()
+        {
+            context.SaveChanges();
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
