00a43b1a992e82b026d3d4ea50ce0ce760547fec
herrera10 <41028500+herrera10@users.noreply.github.com>
2019-03-17 17:02:51 -0600

    Update de Genero Producto y Medidas del Producto
    Update de Genero Producto y Medidas del Producto

---
+++ ProyectoFinal/UnitTestProject/TestGeneroProducto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestGeneroProducto
+    {
+
+
+        [TestMethod]
+        public void agregarGeneroProducto()
+        {
+
+
+            using (UnidadDeTrabajo<genero_producto> unidad = new UnidadDeTrabajo<genero_producto>(new BDContext()))
+            {
+
+                genero_producto genero_Producto = new genero_producto
+                {
+                   productoId = 1,
+                   genero = "Unisex"
+
+                };
+
+                unidad.genericDAL.Add(genero_Producto);
+                unidad.Complete();
+            }
+
+        }
+
+
+        [TestMethod]
+
+        public void actualizarGeneroProducto()
+        {
+
+            using (UnidadDeTrabajo<genero_producto> unidad = new UnidadDeTrabajo<genero_producto>(new BDContext()))
+            {
+                genero_producto genero_Producto = new genero_producto
+                {
+                    productoId = 1,
+                    genero = "Unisexx"
+
+                };
+
+                unidad.genericDAL.Update(genero_Producto);
+                unidad.Complete();
+            }
+
+        }
+
+        [TestMethod]
+
+        public void borrarGeneroProducto()
+        {
+            genero_producto genero_Producto;
+
+            using (UnidadDeTrabajo<genero_producto> unidad = new UnidadDeTrabajo<genero_producto>(new BDContext()))
+            {
+                genero_Producto = unidad.genericDAL.Get(1);
+                unidad.genericDAL.Remove(genero_Producto);
+                unidad.Complete();
+            }
+
+        }
+
+
+    }
+
+
+}
---
+++ ProyectoFinal/UnitTestProject/TestMedidaProducto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestMedidaProducto
+    {
+
+
+        [TestMethod]
+        public void agregarMedidaProducto()
+        {
+
+
+            using (UnidadDeTrabajo<medida_producto> unidad = new UnidadDeTrabajo<medida_producto>(new BDContext()))
+            {
+
+                medida_producto medida_Producto = new medida_producto
+                {
+                    productoId = 1,
+                    medida = 5.5
+
+                };
+
+                unidad.genericDAL.Add(medida_Producto);
+                unidad.Complete();
+            }
+
+        }
+
+
+        [TestMethod]
+
+        public void actualizarMedidaProducto()
+        {
+
+            using (UnidadDeTrabajo<medida_producto> unidad = new UnidadDeTrabajo<medida_producto>(new BDContext()))
+            {
+                medida_producto medida_Producto = new medida_producto
+                {
+                    productoId = 1,
+                    medida = 5.8
+                   
+
+                };
+
+                unidad.genericDAL.Update(medida_Producto);
+                unidad.Complete();
+            }
+
+        }
+
+        [TestMethod]
+
+        public void borrarMedidaProducto()
+        {
+            medida_producto medida_Producto;
+
+            using (UnidadDeTrabajo<medida_producto> unidad = new UnidadDeTrabajo<medida_producto>(new BDContext()))
+            {
+                medida_Producto = unidad.genericDAL.Get(1);
+                unidad.genericDAL.Remove(medida_Producto);
+                unidad.Complete();
+            }
+
+        }
+
+
+    }
+
+
+}
