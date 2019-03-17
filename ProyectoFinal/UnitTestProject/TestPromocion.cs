0f74ee750a0418e7f2f91e32991b9ed612c5b325
herrera10 <41028500+herrera10@users.noreply.github.com>
2019-03-17 16:48:14 -0600

    Update Promociones
    Update Promociones

---
+++ ProyectoFinal/UnitTestProject/TestPromocion.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestPromocion
+    {
+
+
+        [TestMethod]
+        public void agregarPromocion()
+        {
+
+
+            using (UnidadDeTrabajo<promociones> unidad = new UnidadDeTrabajo<promociones>(new BDContext()))
+            {
+
+                promociones promocion = new promociones
+                {
+                    nombre = "Segundo Articulo 20%",
+                    descripcion = "Celulares S7,S8 20% de descuento",
+                    valor = 20
+
+                };
+
+                unidad.genericDAL.Add(promocion);
+                unidad.Complete();
+            }
+
+        }
+
+
+        [TestMethod]
+
+        public void actualizarPromocion()
+        {
+
+            using (UnidadDeTrabajo<promociones> unidad = new UnidadDeTrabajo<promociones>(new BDContext()))
+            {
+                promociones promocion = new promociones
+                {
+                    promocionId = 3,
+                    nombre = "Segundo Articulo 20%",
+                    descripcion = "Celulares S7,S8,S9 20% de descuento",
+                    valor = 20
+
+                };
+
+                unidad.genericDAL.Update(promocion);
+                unidad.Complete();
+            }
+
+        }
+
+        [TestMethod]
+
+        public void borrarPromocion()
+        {
+            promociones promocion;
+
+            using (UnidadDeTrabajo<promociones> unidad = new UnidadDeTrabajo<promociones>(new BDContext()))
+            {
+                promocion = unidad.genericDAL.Get(1);
+                unidad.genericDAL.Remove(promocion);
+                unidad.Complete();
+            }
+
+        }
+
+
+    }
+
+
+}
