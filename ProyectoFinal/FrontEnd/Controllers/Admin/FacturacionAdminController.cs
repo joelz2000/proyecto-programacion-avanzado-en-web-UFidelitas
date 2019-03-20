51ab712b2609030c78a751d4ebbc05c045679dd4
Joel Zúñiga Jiménez <joelz2000@live.com>
2019-03-20 10:25:33 -0600

    agregando facturaciones-admin-frontend

---
+++ ProyectoFinal/FrontEnd/Controllers/Admin/FacturacionAdminController.cs
@@ -0,0 +1,123 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers.Admin
+{
+    public class FacturacionAdminController : Controller
+    {
+        // GET: Facturacion
+        public ActionResult Index()
+        {
+            string mensaje = "";
+            if (Session["mensaje"] != null)
+            {
+                mensaje = Session["mensaje"].ToString();
+            }
+            List<sp_obtenerFacturaciones_Result> facturaciones;
+
+            IFacturacionDAL facturacionDAL = new FacturacionDALImpl();
+
+            facturaciones = facturacionDAL.obtenerFacturacion().ToList();
+
+            List<FacturacionesViewModels> facturacionesVM = new List<FacturacionesViewModels>();
+
+            FacturacionesViewModels facturacionVM;
+
+            foreach (var item in facturaciones)
+            {
+                facturacionVM = new FacturacionesViewModels
+                {
+                    facturacionId = item.facturacionId,
+                    nombre = item.nombre,
+                    fecha = item.fecha,
+                    descripcion = item.descripcion,
+                    impuesto = item.impuesto,
+                    subtotal = item.subtotal,
+                    total = item.total,
+                    tipo = item.tipo
+                };
+
+                facturacionesVM.Add(facturacionVM);
+            }
+            return View("~/Views/Admin/FacturacionAdmin/Index.cshtml", facturacionesVM);
+        }
+
+        // GET: Facturacion/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Facturacion/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Facturacion/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Facturacion/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Facturacion/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Facturacion/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Facturacion/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProyectoFinal/FrontEnd/Models/FacturacionesViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    public class FacturacionesViewModels
+    {
+        [Key]
+        [Display(Name = "Id")]
+        public int facturacionId { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required]
+        public string nombre { get; set; }
+
+        [Display(Name = "Fecha de compra")]
+        [Required]
+        public Nullable<System.DateTime> fecha { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string descripcion { get; set; }
+
+
+        [Display(Name = "Impuesto")]
+        [Required]
+        public Nullable<int> impuesto { get; set; }
+
+
+        [Display(Name = "Subtotal")]
+        public Nullable<double> subtotal { get; set; }
+
+
+        [Display(Name = "Total")]
+        public Nullable<double> total { get; set; }
+
+
+        [Display(Name = "Tipo")]
+        public string tipo { get; set; }
+    }
+}
