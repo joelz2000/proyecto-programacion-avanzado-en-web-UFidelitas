208cef9fcb895454a7ce57d6a26aef46f56ca80c
Gabriel Umana Frias <gabrieluf2297@gmail.com>
2019-03-27 21:59:20 -0600

    Implementando sweetalert2

--- ProyectoFinal/FrontEnd/App_Start/BundleConfig.cs
+++ ProyectoFinal/FrontEnd/App_Start/BundleConfig.cs
@@ -8,11 +8,16 @@ namespace FrontEnd
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/adminlte").Include(
-                        "~/Content/dist/js/adminlte.min.js"));
+           bundles.Add(new ScriptBundle("~/bundles/JSAdmin").Include(
+                        "~/Scripts/jquery.min.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Content/dist/js/adminlte.min.js",
+                        "~/Content/bower_components/datatables.net/js/jquery.dataTables.min.js",
+                        "~/Content/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js",
+                        "~/Scripts/sweetalert2.min.js"));
             
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.slim.min.js"));
+                        "~/Scripts/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,11 +29,11 @@ namespace FrontEnd
             bundles.Add(new ScriptBundle("~/bundles/popper").Include(
                         "~/Scripts/umd/popper.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+            bundles.Add(new ScriptBundle("~/bundles/adminlte").Include(
                         "~/dist/js/adminlte.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/sweetalert").Include(
-                        "~/Scripts/SweetAlert/seetalert2.min.js"));
+                        "~/Scripts/sweetalert2.min.js"));
 
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -41,20 +46,8 @@ namespace FrontEnd
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
-                      "~/Content/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css",
                       "~/Content/theme.min.css",
-                      "~/Content/SweetAlert/sweetalert2.mim.css"));
-
-            bundles.Add(new StyleBundle("~/Content/AdminCss").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/dist/css/AdminLTE.min.css",
-                      "~/Content/dist/css/skins/_all-skins.min.css",
-                      "~/Content/SweetAlert/sweetalert2.mim.css",
-                      "~/Content/estilos.css",
-                      "~/Content/bower_components/font-awesome/css/font-awesome.min.css",
-                      "~/Content/bower_components/Ionicons/css/ionicons.min.css"));
-            
-
+                      "~/Content/SweetAlert/sweetalert2.min.css"));
         }
     }
 }
