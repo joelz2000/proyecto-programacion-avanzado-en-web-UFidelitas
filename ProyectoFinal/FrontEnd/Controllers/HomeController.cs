722e430743c83f711ed556c0a20e838f202a7eaa
Gabriel Umana Frias <gabrieluf2297@gmail.com>
2019-03-28 17:10:25 -0600

    Agregando productos para usuario

--- ProyectoFinal/FrontEnd/Controllers/HomeController.cs
+++ ProyectoFinal/FrontEnd/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using BackEnd.DAL;
 using BackEnd.Entities;
+using FrontEnd.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,14 @@ namespace FrontEnd.Controllers
     {
         private static BDContext context = new BDContext();
         private UnidadDeTrabajo<usuarios> unidad_Usuario = new UnidadDeTrabajo<usuarios>(context);
+
+        // unidades de trabajo para obtener datos necesarios
+        private UnidadDeTrabajo<productos> unidad_productos = new UnidadDeTrabajo<productos>(new BDContext());
+        private UnidadDeTrabajo<categorias> unidad_categorias = new UnidadDeTrabajo<categorias>(new BDContext());
+        private UnidadDeTrabajo<marcas> unidad_marcas = new UnidadDeTrabajo<marcas>(new BDContext());
+        private UnidadDeTrabajo<colecciones> unidad_colecciones = new UnidadDeTrabajo<colecciones>(new BDContext());
+        private UnidadDeTrabajo<distribuidor> unidad_distribuidor = new UnidadDeTrabajo<distribuidor>(new BDContext());
+
         public ActionResult Index()
         {
             // revisar si el usuario no es administrador
@@ -24,8 +33,43 @@ namespace FrontEnd.Controllers
             // usuario no es admin, continuar
             else
             {
-                // usuarios usuario = context.usuarios.Where(u => u.Usuario_ID.Equals(User.Identity.GetUserId())).Single();
-                return View();
+                List<productos> lista_productos = unidad_productos.genericDAL.GetAll().ToList();
+                List<marcas> lista_marcas = unidad_marcas.genericDAL.GetAll().ToList();
+                IndexProductoViewModels producto_VM;
+                List<IndexProductoViewModels> lista_productos_VM = new List<IndexProductoViewModels>();
+
+                // asignar valores correspondientes
+                foreach (var producto in lista_productos)
+                {
+                    // no tomar en cuenta los productos con estado bloqueado
+                    if (producto.id_estado == 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        foreach (var marca in lista_marcas)
+                        {
+                            if (marca.id_marca == producto.id_marca)
+                            {
+                                producto_VM = new IndexProductoViewModels()
+                                {
+                                    Id_Producto = producto.productoId,
+                                    Nombre = producto.nombre,
+                                    Precio = producto.precio,
+                                    Descripcion = producto.descripcion,
+                                    Modelo = producto.modelo,
+                                    cantidad = producto.cantidad,
+                                    marca = marca.nombre
+                                };
+                                lista_productos_VM.Add(producto_VM);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return View(lista_productos_VM);
             }
         }
 
---
+++ ProyectoFinal/FrontEnd/Controllers/User/ProductosController.cs
@@ -0,0 +1,69 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers.User
+{
+    public class ProductosController : Controller
+    {
+
+        // unidades de trabajo para obtener datos necesarios
+        private UnidadDeTrabajo<productos> unidad_productos = new UnidadDeTrabajo<productos>(new BDContext());
+        private UnidadDeTrabajo<categorias> unidad_categorias = new UnidadDeTrabajo<categorias>(new BDContext());
+        private UnidadDeTrabajo<marcas> unidad_marcas = new UnidadDeTrabajo<marcas>(new BDContext());
+        private UnidadDeTrabajo<colecciones> unidad_colecciones = new UnidadDeTrabajo<colecciones>(new BDContext());
+        private UnidadDeTrabajo<distribuidor> unidad_distribuidor = new UnidadDeTrabajo<distribuidor>(new BDContext());
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: HomeAdmin/Details/5
+        public ActionResult Detalles(int? id)
+        {
+            // revisar si el URL contiene un ID, si no entonces devolver 404
+            if (id == null || id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // buscar el producto y los demas datos
+            productos producto = unidad_productos.genericDAL.Get((int)id);
+            marcas marca = unidad_marcas.genericDAL.Get((int)producto.id_marca);
+            colecciones coleccion = unidad_colecciones.genericDAL.Get((int)producto.id_coleccion);
+            categorias categoria = unidad_categorias.genericDAL.Get((int)producto.id_categoria);
+            distribuidor distribuidor = unidad_distribuidor.genericDAL.Get((int)producto.id_distribuidor);
+
+            // ver si el producto tiene estado bloqueado. Si si, devolver 404
+            if (producto.id_estado == 1)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // asignar datos al view model correspondiente
+            DetallesProductoViewModels producto_VM = new DetallesProductoViewModels
+            {
+                Id_Producto = producto.productoId,
+                Nombre = producto.nombre,
+                Precio = producto.precio,
+                Descripcion = producto.descripcion,
+                Modelo = producto.modelo,
+                cantidad = producto.cantidad,
+                marca = marca.nombre,
+                coleccion = coleccion.nombre,
+                categoria = categoria.nombre,
+                distribuidor = distribuidor.nombre
+            };
+
+            return View(producto_VM);
+        }
+    }
+}
