4cf037a53916b6325d3da356cdf93adc08b31e3c
herrera10 <41028500+herrera10@users.noreply.github.com>
2019-04-14 17:20:49 -0600

    agregando compra

---
+++ ProyectoFinal/FrontEnd/Controllers/User/CompraController.cs
@@ -0,0 +1,244 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers.User
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Compra/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Compra/Create
+        public ActionResult Create()
+        {
+            ComprasViewModels CompraVM = new ComprasViewModels();
+
+            List<Pais> paises = new List<Pais>();
+            List<Provincia> provincias = new List<Provincia>();
+            List<Canton> cantones = new List<Canton>();
+            List<Distrito> distritos = new List<Distrito>();
+
+            //lista paises
+            using (UnidadDeTrabajo<Pais> unidad = new UnidadDeTrabajo<Pais>(new BDContext()))
+            {
+
+                paises = unidad.genericDAL.GetAll().ToList();
+
+            }
+
+            foreach (var pais in paises)
+            {
+                if (pais.id_estado != 1)
+                {
+                    CompraVM.lista_pais.Add(new SelectListItem()
+                    {
+                        Text = pais.nombre,
+                        Value = pais.paisId.ToString()
+                    });
+
+
+                }
+            }
+
+            //lista provincias
+            using (UnidadDeTrabajo<Provincia> unidad = new UnidadDeTrabajo<Provincia>(new BDContext()))
+            {
+
+                provincias = unidad.genericDAL.GetAll().ToList();
+
+            }
+
+            foreach (var provincia in provincias)
+            {
+                if (provincia.id_estado != 1)
+                {
+                    CompraVM.lista_provincia.Add(new SelectListItem()
+                    {
+                        Text = provincia.nombre,
+                        Value = provincia.provinciaId.ToString()
+                    });
+
+
+                }
+            }
+
+            //lista cantones
+            using (UnidadDeTrabajo<Canton> unidad = new UnidadDeTrabajo<Canton>(new BDContext()))
+            {
+
+                cantones = unidad.genericDAL.GetAll().ToList();
+
+            }
+
+            foreach (var canton in cantones)
+            {
+                if (canton.id_estado != 1)
+                {
+                    CompraVM.lista_canton.Add(new SelectListItem()
+                    {
+                        Text = canton.nombre,
+                        Value = canton.cantonId.ToString()
+                    });
+
+
+                }
+            }
+
+
+            //lista distrito
+            using (UnidadDeTrabajo<Distrito> unidad = new UnidadDeTrabajo<Distrito>(new BDContext()))
+            {
+
+                distritos = unidad.genericDAL.GetAll().ToList();
+
+            }
+
+            foreach (var distrito in distritos)
+            {
+                if (distrito.id_estado != 1)
+                {
+                    CompraVM.lista_distrito.Add(new SelectListItem()
+                    {
+                        Text = distrito.nombre,
+                        Value = distrito.distritoId.ToString()
+                    });
+
+
+                }
+            }
+
+
+
+            return View("~/Views/User/Compra/Create.cshtml", CompraVM);
+        }
+
+        // POST: Compra/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ComprasViewModels comprasViewModels)
+        {
+            try
+            {
+
+                // Crear factura
+                IFacturacionDAL facturacionDAL = new FacturacionDALImpl();
+                sp_obtenerFacturaciones_Result facturacion;
+                facturacionDAL.agregarFactura(facturacion = new sp_obtenerFacturaciones_Result()
+                {
+                    nombre = "facturacion digital",
+                    fecha = DateTime.UtcNow,
+                    descripcion = null,
+                    impuesto = 13,
+                    subtotal = null,
+                    total = null,
+                    tipo = "Digital",
+                    id_estado = 2
+                });
+
+                //obtener la ultima factura
+
+                int idFactura;
+
+                idFactura = facturacionDAL.obtenerFacturacion().Last().facturacionId;
+
+
+                //agregar productos a facturacion
+                List<carrito> carritos;
+
+                using (UnidadDeTrabajo<carrito> unidad = new UnidadDeTrabajo<carrito>(new BDContext()))
+                {
+                    carritos = unidad.genericDAL.GetAll().ToList();
+                }
+
+                IProductosFacturacionDAL productosFacturacionDAL = new ProductosFacturacionDALImpl();
+
+                sp_obtenerFacturacionesProducto_Result facturacionProducto;
+
+                foreach (var carrito in carritos)
+                {
+                    facturacionProducto = new sp_obtenerFacturacionesProducto_Result
+                    {
+                        productoId = carrito.productoId,
+                        facturacionId = idFactura,
+                        cantidad = carrito.cantidad_producto,
+                        id_estado = 2
+                    };
+
+                    productosFacturacionDAL.agregarProductoFacturacion(facturacionProducto);
+
+
+                }
+
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+
+
+
+                
+            }
+            catch
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        // GET: Compra/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Compra/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Compra/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Compra/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProyectoFinal/FrontEnd/Models/ComprasViewModels.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Models
+{
+    public class ComprasViewModels
+    {
+        [Key]
+        [Display(Name = "Usuario")]
+        public int idUsuario { get; set; }
+
+        [Key]
+        [Display(Name = "Producto")]
+        public int idProducto { get; set; }
+
+
+        [Display(Name = "Cantidad")]
+        public Nullable<int> cantidad { get; set; }
+
+       
+        [Display(Name = "Fecha de compra")]
+        [Required]
+        public Nullable<System.DateTime> fecha { get; set; }
+
+        [Key]
+        [Display(Name = "Facturacion")]
+        public int idFacturacion { get; set; }
+
+        [Display(Name = "Telefono")]
+        public int telefono { get; set; }
+
+        [Display(Name = "Direccion")]
+        public string direccion { get; set; }
+
+        [Key]
+        [Display(Name = "Pais")]
+        public int idPais { get; set; }
+
+        [Key]
+        [Display(Name = "Provincia")]
+        public int idProvincia { get; set; }
+
+        [Key]
+        [Display(Name = "Canton")]
+        public int idCanton { get; set; }
+
+        [Key]
+        [Display(Name = "Distrito")]
+        public int idDistrito { get; set; }
+
+        public List<SelectListItem> lista_pais = new List<SelectListItem>();
+        public List<SelectListItem> lista_provincia = new List<SelectListItem>();
+        public List<SelectListItem> lista_canton = new List<SelectListItem>();
+        public List<SelectListItem> lista_distrito = new List<SelectListItem>();
+
+
+
+
+
+    }
+}
